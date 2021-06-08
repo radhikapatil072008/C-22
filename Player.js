f99c602f664f710937f5d53ef73b8edeff184e1b
radhikapatil072008 <84386599+radhikapatil072008@users.noreply.github.com>
2021-06-08 16:47:22 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,50 @@
+class Player
+{
+    constructor() {
+		this.x = 500;
+		this.y=200;
+		this.spt=createSprite(this.x, this.y, 100,100);
+		this.spt.shapeColor="blue";
+
+	}
+
+  applyGravity()
+    {
+        this.spt.velocityY=this.spt.velocityY+2;
+	}
+  moveRight(){
+  this.spt.x=this.spt.x+10
+  }	
+
+  moveLeft(){
+	this.spt.x=this.spt.x-10
+  }
+
+  jump(){
+  this.spt.velocityY=-12
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
